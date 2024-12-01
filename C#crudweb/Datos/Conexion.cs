3131267bd1f832151e870e148443888dd409a2e5
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-12-01 20:00:56 -0300

    feat: get conexion sql

---
+++ C#crudweb/Datos/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace C_crudweb.Datos
+{
+    public class Conexion
+    {
+        private string CadenaSQL = string.Empty;
+
+        public Conexion() { 
+            
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+        }
+    }
+}
