b10246a625be9c40ad4e8af36ff833055223cc92
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-12-01 02:25:49 -0300

    feat: clase contacto

---
+++ C#crudweb/Models/ContactoModelo.cs
@@ -0,0 +1,10 @@
+﻿namespace C_crudweb.Models
+{
+    public class ContactoModelo
+    {
+        public int IdContacto { get; set; }
+        public string? Nombre { get; set; }
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
+    }
+}
