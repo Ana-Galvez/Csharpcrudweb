d1c7cd79ba3b3e942495b49541b4a072cee0a718
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-12-02 01:52:56 -0300

    feat: creación primer formulario parte1

--- C#crudweb/Program.cs
+++ C#crudweb/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Mantenedor}/{action=GuardarLista}/{id?}");
 
 app.Run();
