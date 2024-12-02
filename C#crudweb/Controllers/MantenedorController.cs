c16bec28fe4b6d1b329ddf9a27f032c4699b3539
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-12-02 01:24:46 -0300

    feat: metodos de vistas

---
+++ C#crudweb/Controllers/MantenedorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using C_crudweb.Datos;
+using C_crudweb.Models;
+
+
+namespace C_crudweb.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        ContactoDatos contactoDatos=new ContactoDatos();
+        public IActionResult Listar()
+        {
+            var objLista = contactoDatos.Listar();
+            return View(objLista);
+        } 
+        public IActionResult GuardarLista()
+        {
+            return View();
+        }          
+        public IActionResult Guardar(ContactoModelo objContacto)
+        {
+            var respuesta = contactoDatos.GuardarContacto(objContacto);
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }        
+        public IActionResult Modificar()
+        {
+            return View();
+        }        
+        public IActionResult Eliminar()
+        {
+            return View();
+        }
+    }
+}
